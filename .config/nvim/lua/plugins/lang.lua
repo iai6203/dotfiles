3d1321830e9882e7c00ac5dbfab5474ee0992482
김재석 <60083524+iai6203@users.noreply.github.com>
2026-01-15 14:16:12 +0900

    feat(nvim): render-markdown 설정 업데이트 (#26)

--- .config/nvim/lua/plugins/lang.lua
+++ .config/nvim/lua/plugins/lang.lua
@@ -4,6 +4,13 @@ return {
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {
+      heading = {
+        sign = true,
+        icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+      },
+      code = {
+        sign = true,
+      },
       win_options = {
         conceallevel = { default = 0, rendered = 3 },
       },
