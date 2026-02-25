dd572a3b64d546a985bd5f26eabd3b0396dec0b8
김재석 <iai6203@gmail.com>
2026-02-25 15:17:32 +0900

    feat(scripts): ide.sh 업데이트

--- scripts/ide.sh
+++ scripts/ide.sh
@@ -4,3 +4,7 @@ tmux rename-window 'ide'
 tmux send-keys 'nvim' Enter
 tmux split-window -h -p 30
 tmux send-keys 'claude' Enter
+tmux split-window -v -p 67
+tmux send-keys 'claude' Enter
+tmux split-window -v -p 50
+tmux send-keys 'claude' Enter
