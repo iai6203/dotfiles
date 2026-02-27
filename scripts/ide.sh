6ec06b80d433af1243ed1e2d5195cd97eb489009
김재석 <iai6203@gmail.com>
2026-02-27 09:53:31 +0900

    feat(scripts): ide.sh 레이아웃 변경

--- scripts/ide.sh
+++ scripts/ide.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 현재 윈도우 이름을 'ide'로 변경
-tmux rename-window 'ide'
-
 # 좌측 pane에서 nvim 실행
 tmux send-keys 'nvim' Enter
 
@@ -14,3 +11,9 @@ tmux send-keys 'claude' Enter
 
 # 좌측 pane(nvim)으로 포커스 이동
 tmux select-pane -L
+
+# 하단에 10% 크기의 pane 생성
+tmux split-window -v -p 10
+
+# 상단 pane으로 포커스 이동
+tmux select-pane -U
